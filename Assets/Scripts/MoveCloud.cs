2d0589f5d6f8856c712918f39f9ed806abea88cc
Magnus <mwpe16@student.aau.dk>
2019-11-24 10:59:46 +0100

    Added destruction by death cloud

--- Assets/Scripts/MoveCloud.cs
+++ Assets/Scripts/MoveCloud.cs
@@ -6,16 +6,25 @@ public class MoveCloud : MonoBehaviour
 {
 
     public float cloudSpeed;
+    public bool wait;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right* cloudSpeed * Time.deltaTime, Space.Self);
+        transform.Translate(Vector3.right * cloudSpeed * Time.deltaTime, Space.Self);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.Destroy();
+        }
     }
 }
