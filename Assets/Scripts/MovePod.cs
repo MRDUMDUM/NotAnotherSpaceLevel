3e087d6d1d1872cc04cf993e963a85e9e03259ef
Unknown <dav7id@hotmail.com>
2019-11-24 10:51:40 +0100

    Auto stash before merge of "master" and "Github/master"

---
+++ Assets/Scripts/MovePod.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePod : MonoBehaviour
+{
+    public float podSpeed;
+    public bool finish = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (finish)
+        {
+            transform.Translate(Vector3.right * podSpeed * Time.deltaTime, Space.Self);
+        }
+        
+    }
+}
